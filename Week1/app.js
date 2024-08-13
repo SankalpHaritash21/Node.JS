71882e6d780995a1f5eb774ee63d31e88330d797
Sankalp <sanklpkaushik21@gmail.com>
2024-08-13 06:36:45 +0530

    Fiste Node.JS Code

---
+++ Week1/app.js
@@ -0,0 +1,13 @@
+var x = "Sankalp Haritash";
+
+var a = 1;
+var b = 2;
+
+console.log(x);
+console.log(a + b);
+
+console.log(this);
+
+console.log(globalThis);
+
+console.log(globalThis === global);
