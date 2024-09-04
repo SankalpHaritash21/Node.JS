7672707ca9d78943d2442c630036a0ac23be75bb
Sankalp <sanklpkaushik21@gmail.com>
2024-09-04 21:48:50 +0530

    Node Js Express

--- week5/Gemini/geimini.js
+++ week5/Gemini/geimini.js
@@ -1,6 +1,15 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(express.json());
+app.use(bodyParser.json());
 // Make sure to include these imports:
 // import { GoogleGenerativeAI } from "@google/generative-ai";
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 require("dotenv").config();
 const genAI = new GoogleGenerativeAI(process.env.API_Key);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
@@ -11,9 +20,26 @@ const Generate = async () => {
 
     const result = await model.generateContent(prompt);
     console.log(result.response.text());
+    return result.response.text();
   } catch (error) {
     console.log(error);
   }
 };
 
-Generate();
+// Generate();
+
+app.get("/api/constent", async (req, res) => {
+  try {
+    const d = req.body.question;
+    const r = await Generate();
+    res.send({
+      result: r,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on Port: 3000");
+});
